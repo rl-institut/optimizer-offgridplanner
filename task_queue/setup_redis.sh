ad7b4ceb88a94174cba64082c5d14496088d68e1
pierre-francois.duc <pierre-francois.duc@rl-institut.de>
2025-04-22 11:28:55 +0200

    Update local redis setup

--- task_queue/setup_redis.sh
+++ task_queue/setup_redis.sh
@@ -3,4 +3,4 @@ export CELERY_BROKER_URL="redis://localhost:6379/0"
 export CELERY_RESULT_BACKEND="redis://localhost:6379/0"
 export C_FORCE_ROOT="true"
 export BROKER_URL="redis://localhost:6379/0"
-celery -A tasks worker --loglevel=info
+celery -A tasks worker --loglevel=info --queues=supply -E
